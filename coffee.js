1f5adf9bda4e11eda463e55bb5d61746f6266544
ADEIKA, Blessing Isoyiza <blessadeik@gmail.com>
2021-05-25 17:13:03 +0100

    add

---
+++ coffee.js
@@ -0,0 +1,10 @@
+// Location - google maps
+
+// function myMap() {
+// var mapProp= {
+//   center:new google.maps.LatLng(7.398980,3.920400),
+//   zoom:5,
+// };
+// var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+// }
